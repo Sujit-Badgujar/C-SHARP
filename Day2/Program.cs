480825a5026ed6c777f5b3b936ee78c5596006f5
Sujit J. Badgujar <sujitbadgujar001@gmail.com>
2024-10-04 22:44:08 +0530

    Add project files.

---
+++ Day2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch case
+            //System.Console.WriteLine("Enter the marks obtained:");
+            //int gradeMarks = int.Parse(Console.ReadLine());
+
+            //string Remark;
+
+            //switch (gradeMarks)
+            //{
+            //    case 0:Remark = "Low"; break;
+            //    case 50:Remark = "good"; break;
+            //    case 100:Remark = "best"; break;
+            //    case 75:Remark = "better"; break;
+            //    default:Remark = "None"; break;
+            //}
+            //System.Console.WriteLine(Remark);
+            //System.Console.ReadKey();
+
+            // While
+            //int a = 1;
+            //while (a<=1000)
+            //{
+            //    Console.Write(" ,"+a);
+            //    a++;
+            //}
+            //System.Console.ReadKey();
+
+            //Do-while,
+            //int b = 1001;
+            //do
+            //{
+            //    Console.Write(" ," + b);
+            //    b++;
+            //} while (b <= 1000);
+            //System.Console.ReadKey();
+
+
+            //For-Loop
+            for(int i=0;i<11;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey(); 
+            //nested for 
+            //Break-Continue
+            //goto
+
+        }
+    }
+}
