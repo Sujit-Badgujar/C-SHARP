e3cc0ae320168f36acfb355bb8ca15666e90c353
Sujit <112996880+Sujit-Badgujar@users.noreply.github.com>
2024-10-10 08:49:48 +0530

    Add files via upload

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hi Sujit, How r u?");
+
+            //Area of circle
+            double pie = 3.14;
+            int r = 10;
+            double area = 0;
+
+            area = pie * (r * r);
+
+            System.Console.Write("Area of circle is : "+area);
+            System.Console.ReadKey();
+
+            //Done till if else loops
+
+        }
+    }
+}
