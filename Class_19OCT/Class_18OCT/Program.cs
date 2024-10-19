fdfd9d72ff0e7e0959fb42f312c1bbe77e67b938
Sujit.Dev <112996880+Sujit-Badgujar@users.noreply.github.com>
2024-10-19 22:34:42 +0530

    Updated on 19thOCT

---
+++ Class_19OCT/Class_18OCT/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_18OCT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees = new List<Employee>
+            {
+                new Employee(1,"Sujit", "IT", 900000),
+                new Employee(2,"Sumit", "IT", 120000),
+                new Employee(3,"Om", "IT", 100000),
+                new Employee(4,"Gayatri", "IT", 130000),
+                new Employee(5,"Vaish", "IT", 100000),
+            };
+
+            Console.WriteLine("Employees of Badgujar_Tech:");
+            foreach (var employee in Employees) 
+            {
+                Console.WriteLine(employee);
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
